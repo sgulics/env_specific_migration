2d7bfaa862d6cd9ecdea1550a05f41ad965970cc
Steve Gulics <sgulics@hotmail.com>
2011-03-25 22:08:26 -0400

    removed ugly comments

--- lib/env_specific_migration.rb
+++ lib/env_specific_migration.rb
@@ -7,21 +7,16 @@ module EnvSpecificMigration
     # Will only run a migration if its in the comma delimited list of environment symbols
     def run_migration_only_in(*env)
 
-        # puts "in run_migration_only_in #{env}"
         unless self.respond_to? :migrate_without_filtered_migration
           
-          #puts self.singleton_methods.sort.each{|m| puts m}
           class << self
-             def migrate_with_filtered_migration(direction)
-              # puts "in migrate_with_filtered_migration(#{direction}"
-              #               puts "filtered environments: #{filtered_environments.inspect}"
+            def migrate_with_filtered_migration(direction)
               if filtered_environments.include?(Rails.env.to_sym)
                  puts "-- running environment specific migration"
                  migrate_without_filtered_migration(direction)
               else
                  puts "-- ignoring environment specific migration"
               end
-              #up_without_filterd_migration(&block)
             end
             alias_method_chain :migrate, :filtered_migration
 
