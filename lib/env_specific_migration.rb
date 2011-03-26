00f8404c4c919a82adcdf0fe69b6f02d7a589a26
Steve Gulics <sgulics@hotmail.com>
2011-03-25 21:43:07 -0400

    Added some debugging

--- lib/env_specific_migration.rb
+++ lib/env_specific_migration.rb
@@ -16,10 +16,10 @@ module EnvSpecificMigration
               # puts "in migrate_with_filtered_migration(#{direction}"
               #               puts "filtered environments: #{filtered_environments.inspect}"
               if filtered_environments.include?(Rails.env.to_sym)
-                # puts "running migration"
+                 puts "-- running migration"
                  migrate_without_filtered_migration(direction)
               else
-                 # puts "Ignoring Migration"
+                 puts "-- ignoring migration"
               end
               #up_without_filterd_migration(&block)
             end
