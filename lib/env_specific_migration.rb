d8f5b6786d5f0a726d2dd541862c4aa584815df9
Steve Gulics <sgulics@hotmail.com>
2011-07-21 12:12:50 -0400

    Some refactoring and now supports regular expressions

--- lib/env_specific_migration.rb
+++ lib/env_specific_migration.rb
@@ -1,57 +1,64 @@
 module RailsVersionFilterMethods
-  def rails_3_1_and_up
-    if Rails::VERSION::MAJOR > 3 || (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 1)
-      yield
-    end
+  
+  def rails_3_1_and_up?
+    Rails::VERSION::MAJOR > 3 || (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 1)
   end
 
-  def less_than_rails_3_1
-    if Rails::VERSION::MAJOR < 3 || (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR < 1)
-      yield
-    end
+  def less_than_rails_3_1?
+    Rails::VERSION::MAJOR < 3 || (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR < 1)
   end
 end
 
 #FilteredMigration
 module EnvSpecificMigration  
+
+
+  module SupportMethods
+
+    def migrate_with_filtered_migration(direction)
+      if run_migration?
+        puts "-- running environment specific migration"
+        migrate_without_filtered_migration(direction)
+      else
+        puts "-- ignoring environment specific migration"
+      end
+    end
+
+    def run_migration?
+      result = false
+      self.filtered_environments.each do |e|
+        case e
+        when Regexp
+          result = e =~ Rails.env.to_s
+        when String
+          result = e == Rails.env
+        when Symbol
+          result = e == Rails.env.to_sym
+        end
+        break if result    
+      end
+      result
+    end
+
+  end
+
   module ClassMethods
 
     # Will only run a migration if its in the comma delimited list of environment symbols
     def run_migration_only_in(*env)
       extend RailsVersionFilterMethods
       
-      less_than_rails_3_1 do
-        unless self.respond_to? :migrate_without_filtered_migration
-          class << self
-            def migrate_with_filtered_migration(direction)
-              if filtered_environments.include?(Rails.env.to_sym)
-                puts "-- running environment specific migration"
-                migrate_without_filtered_migration(direction)
-              else
-                puts "-- ignoring environment specific migration"
-              end
-            end
-            alias_method_chain :migrate, :filtered_migration
-          end
-          class_inheritable_accessor :filtered_environments
-        end
-      end
-      
-      # In Rails 3.1, the #migrate method was moved to an instance method. Additionally, class_inheritable_accessor
-      # has been replaced by class_attribute.
-      rails_3_1_and_up do
-        define_method(:migrate_with_filtered_migration) do |direction|
-          if self.class.filtered_environments.include?(Rails.env.to_sym)
-            puts "-- running environment specific migration"
-            migrate_without_filtered_migration(direction)
-          else
-            puts "-- ignoring environment specific migration"
-          end
-        end
+      # In Rails 3.1, the #migrate method was moved to an instance method. 
+      if rails_3_1_and_up?
+        include EnvSpecificMigration::SupportMethods
         alias_method_chain :migrate, :filtered_migration
-        class_attribute :filtered_environments
+      else
+        class << self
+          include EnvSpecificMigration::SupportMethods
+          alias_method_chain :migrate, :filtered_migration 
+        end   
       end
-      
+      class_inheritable_accessor :filtered_environments
       self.filtered_environments = env
     end
   end
